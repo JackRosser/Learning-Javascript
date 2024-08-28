d929f3e0736d97edfeaaa0c9d7939e63062fb47f
Giacomo Rossettini <rossegiac@gmail.com>
2024-08-28 11:51:41 +0200

    submenu

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,20 +2,22 @@
 
 let header = document.querySelector("header");
 let javaScriptMenu = document.getElementById("javascript");
-
+let body = document.querySelector("body");
 // generiamo un menu a comparsa che inizi a esistere quando si clicca su "javascript"
 // lo generiamo con js e gli diamo come classe "header__submenu"
 
 console.log(javaScriptMenu);
 
-let addSubmenu = function (par1, par2, par3) {
+let addSubmenu = function () {
   let ulSubmenu = document.createElement("ul");
   ulSubmenu.className = "header__submenu";
   let liSubmenu1 = document.createElement("li");
   let aLiSubmenu1 = document.createElement("a");
-  aLiSubmenu1.href = "#";
+  aLiSubmenu1.href = "#metodi";
+  aLiSubmenu1.innerText = "metodi";
+  liSubmenu1.appendChild(aLiSubmenu1);
+  ulSubmenu.appendChild(liSubmenu1);
   header.appendChild(ulSubmenu);
-  ulSubmenu.append(liSubmenu1);
 };
 
-addSubmenu("metodi");
+javaScriptMenu.addEventListener("click", addSubmenu);
