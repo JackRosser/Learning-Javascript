628f196610cfab652c17e919a8035c45f8d0c4f3
Giacomo Rossettini <rossegiac@gmail.com>
2024-08-28 10:23:38 +0200

    addmenu

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,6 @@
 //menu a comparsa
 
+let header = document.querySelector("header");
 let javaScriptMenu = document.getElementById("javascript");
 
 // generiamo un menu a comparsa che inizi a esistere quando si clicca su "javascript"
@@ -7,4 +8,14 @@ let javaScriptMenu = document.getElementById("javascript");
 
 console.log(javaScriptMenu);
 
-let addSubmenu = function () {};
+let addSubmenu = function (par1, par2, par3) {
+  let ulSubmenu = document.createElement("ul");
+  ulSubmenu.className = "header__submenu";
+  let liSubmenu1 = document.createElement("li");
+  let aLiSubmenu1 = document.createElement("a");
+  aLiSubmenu1.href = "#";
+  header.appendChild(ulSubmenu);
+  ulSubmenu.append(liSubmenu1);
+};
+
+addSubmenu("metodi");
